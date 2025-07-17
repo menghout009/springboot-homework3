7842990345939d094728817407596fc7cdaf7928
menghout_git <132098927+menghout009@users.noreply.github.com>
2025-07-17 10:21:36 +0700

    Update HardcodedSecrets.java

--- src/test/java/com/example/spring_homework003/HardcodedSecrets.java
+++ src/test/java/com/example/spring_homework003/HardcodedSecrets.java
@@ -1,7 +1,7 @@
-package com.example.demo.security;
+// package com.example.demo.security;
 
-public class HardcodedSecrets {
-    private static final String DB_USERNAME = "admin";
-    private static final String DB_PASSWORD = "SuperSecret123"; // 🔥 Hardcoded
-    private static final String API_KEY = "sk_test_51LZyWlF9x..."; // 🔥 Simulated Stripe key
-}
+// public class HardcodedSecrets {
+//     private static final String DB_USERNAME = "admin";
+//     private static final String DB_PASSWORD = "SuperSecret123"; // 🔥 Hardcoded
+//     private static final String API_KEY = "sk_test_51LZyWlF9x..."; // 🔥 Simulated Stripe key
+// }
